11f3fb681640b997ab3ad8f03c1d56781d464918
Jarrod Swart <jcswart@gmail.com>
2016-12-18 14:08:14 -0500

    Refactor.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use std::fs::File;
 use std::io::Read;
+use ParseToken as PT;
 
 fn main() {
-    match slurp("test.lisp") {
+    match slurp("test2.lisp") {
         Some(txt) => {
             let mut ts = Vec::new();
             ts = txt.chars().map(|c| parse(c)).collect();
@@ -12,34 +13,38 @@ fn main() {
     }
 }
 
+/// Slurp a file like clojure.
 fn slurp(file: &str) -> Option<String> {
     let mut s = String::new();
-    let f     = File::open(file).ok();
-    if f.is_some() {
-        f.unwrap().read_to_string(&mut s);
-        Some(s)
-    } else {
-        None
-    }
+    File::open(file)
+        .ok()
+        .and_then(|mut x| x.read_to_string(&mut s).ok())
+        .and(Some(s))
 }
 
-fn parse (c: char) -> Token {
+/// Convert the characters in a file into ParseTokens.
+///
+/// The tokens add the first level of structure to the file and will be passed
+/// along to a statemachine to build up the AST.
+fn parse (c: char) -> ParseToken {
     match c {
-       '(' => Token::BeginExpr,
-       ')' => Token::EndExpr,
-       ' ' => Token::Space,
-       '\n'=> Token::Newline,
-       _   => Token::Lookup(c)
+       '(' => PT::BeginExpr,
+       ')' => PT::EndExpr,
+       ' ' => PT::Space,
+       '\n'=> PT::Newline,
+       '\t'=> PT::Tab,
+       _   => PT::Unfinished(c)
     }
 }
 
 #[derive(Debug)]
-enum Token {
+enum ParseToken {
     BeginExpr,
     EndExpr,
     Space,
     Newline,
-    Lookup(char),
+    Tab,
+    Unfinished(char),
 }
 
 #[test]
