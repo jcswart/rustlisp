c870988af8e9a95c6976453a049aac6447e1266d
Jarrod Swart <jcswart@gmail.com>
2016-12-19 11:21:18 -0500

    WIP.

--- src/main.rs
+++ src/main.rs
@@ -5,21 +5,11 @@ use ParseToken as PT;
 fn main() {
     match slurp("test2.lisp") {
         Some(txt) => {
-            let mut ts = Vec::new();
-            ts = txt.chars().map(|c| parse(c)).collect();
+            let ts: Vec<ParseToken> = txt.chars().map(|c| parse(c)).collect();
             println!("{:?}", ts);
         }
-        _ => {}
+        _ => {println!("{}", "No file found.")}
     }
-}
-
-/// Slurp a file like clojure.
-fn slurp(file: &str) -> Option<String> {
-    let mut s = String::new();
-    File::open(file)
-        .ok()
-        .and_then(|mut x| x.read_to_string(&mut s).ok())
-        .and(Some(s))
 }
 
 /// Convert the characters in a file into ParseTokens.
@@ -47,7 +37,30 @@ enum ParseToken {
     Unfinished(char),
 }
 
+fn stringify (xs: Vec<ParseToken>) -> String {
+    xs.iter().map(|x| match x { &PT::Unfinished(c) => Some(c), _ => None})
+             .map(|x| x.unwrap())
+             .collect()
+}
+
+/// Slurp a file like clojure.
+fn slurp(file: &str) -> Option<String> {
+    let mut s = String::new();
+    File::open(file)
+        .ok()
+        .and_then(|mut x| x.read_to_string(&mut s).ok())
+        .and(Some(s))
+}
+
 #[test]
 fn test_slurp() {
     assert_eq!(slurp("test.lisp"), Some(String::from("(+ 1 2)\n")));
+}
+
+#[test]
+fn test_stringify() {
+    assert_eq!(stringify(vec![PT::Unfinished('f'),
+                              PT::Unfinished('o'),
+                              PT::Unfinished('o')]),
+               String::from("foo"));
 }
